441f0fd41c7d0799ca4f67f66c0743c77b927175
nathanmatsubara <nm2834@bard.edu>
2016-08-18 10:26:03 -0400

    Update quote.js

--- workshop1/quote.js
+++ workshop1/quote.js
@@ -4,9 +4,9 @@
  * L&T quote generator
  */
 
-var things = ["Music", "Art", "Dance", "Theater", "Math", "Architecture"];
-var programmers = ["musician", "artist", "choreographer", "director", "mathematician", "architect"]
-var castles = ["museums", "bridges", "cities"]
+var things = ["Sport", "Art", "Dance", "Theater", "Math", "Architecture"];
+var programmers = ["athlete", "artist", "choreographer", "director", "mathematician", "architect"]
+var castles = ["stadium", "bridges", "cities"]
 var exertions = ["effort", "strain", "struggle", "toil", "endeavor", "hard work", "labor"];
 var airs = ["sky", "atmosphere", "heavens", "ether", "wind"];
 
